910cdf98a2def68d78aa1f659ad4deaa7e3fb582
Pejoska99 <biljanapejoska985@gmail.com>
2024-05-18 19:16:25 +0200

    add commit

--- class02 homework/4_constraint.sql
+++ class02 homework/4_constraint.sql
@@ -24,10 +24,43 @@ ADD CONSTRAINT priceConstraint CHECK (price >= cost * 1.20)
 --Update the Product table by adding constraint where all description values will be
 --UNIQUE.
 
+
+----ERROR:  Key (description)=(Gluten Free description) is duplicated.could not create unique index "description_unique_constraint" 
+
+SELECT description, COUNT(*) AS count
+FROM product
+GROUP BY description
+HAVING COUNT(*) > 1;
+
+SELECT * FROM product
+WHERE description = 'Gluten Free description'
+
+DELETE FROM product 
+WHERE id = 13;
+--ERROR:  Key (id)=(13) is still referenced from table "orderdetails".update or delete on table "product" violates foreign key constraint "fk_productid" on table "orderdetails"
+
+UPDATE orderdetails
+SET productid = 4
+WHERE productid = 13;
+
+DELETE FROM product
+WHERE id = 13;
+
+SELECT * FROM product
+WHERE description = 'Multigrain description'
+
+UPDATE orderdetails
+SET productid = 5
+WHERE productid = 12;
+
+DELETE FROM product
+WHERE id = 12;
+
 ALTER TABLE product 
 ADD CONSTRAINT descriptionUniqueConstraint 
 UNIQUE (description);
-----ERROR:  Key (description)=(Gluten Free description) is duplicated.could not create unique index "description_unique_constraint" 
+
+
 
 
 
